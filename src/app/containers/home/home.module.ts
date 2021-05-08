23ba6e7c48bdccfbd0659939cd10c76fbbb54a43
mariom <mariom@empathy.co>
2021-05-08 12:37:23 +0200

    feat(i18n): initial transloco library config

--- src/app/containers/home/home.module.ts
+++ src/app/containers/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 
 import { HomeContainer } from './home.container';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeContainer],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, TranslocoModule],
+  providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'home' }],
 })
 export class HomeModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 
-import { CoreEffects } from './store/core.effects';
+import {
+  TRANSLOCO_CONFIG,
+  TRANSLOCO_LOADER,
+  translocoConfig,
+  TranslocoModule,
+} from '@ngneat/transloco';
+import { TranslateLoaderService } from './services/translate-loader.service';
 import { environment } from 'src/environments/environment';
+import { CoreEffects } from './store/core.effects';
 import * as coreReducer from './store/core.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
+    TranslocoModule,
     StoreModule.forRoot({ core: coreReducer.reducer }),
     EffectsModule.forRoot([CoreEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
+  ],
+  providers: [
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: translocoConfig({
+        availableLangs: ['es'],
+        defaultLang: 'es',
+        prodMode: environment.production,
+      }),
+    },
+    { provide: TRANSLOCO_LOADER, useClass: TranslateLoaderService },
   ],
 })
 export class CoreModule {}
