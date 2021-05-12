c77e7ae68255cd8ef5953efe35ed126d6e5c2444
mariom <mariom@empathy.co>
2021-05-12 14:49:32 +0200

    feat(movie-new): create form and logic for new movie

--- src/app/containers/movies/containers/movie-new/movie-new.container.ts
+++ src/app/containers/movies/containers/movie-new/movie-new.container.ts
@@ -1,12 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Actions, ofType } from '@ngrx/effects';
+import { Subscription } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
+import { ActionTypes, requestCreateMovie } from 'src/app/containers/movies/store/movies.actions';
+import { Movie } from 'src/app/core/models/movies.model';
+import { FormUtilsService } from 'src/app/core/utils/form-utils.service';
+import { Translation, TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-movie-new',
   templateUrl: './movie-new.container.html',
   styleUrls: ['./movie-new.container.scss'],
 })
-export class MovieNewContainer implements OnInit {
-  constructor() {}
+export class MovieNewContainer implements OnInit, OnDestroy {
+  form: FormGroup;
+  translations: Translation;
 
-  ngOnInit() {}
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private store: Store,
+    private actions$: Actions,
+    private fb: FormBuilder,
+    private formUtilsService: FormUtilsService,
+    private translateService: TranslocoService,
+    private toastService: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.createForm();
+
+    this.subscriptions.add(
+      this.translateService
+        .selectTranslateObject('message', {}, 'movieNew')
+        .subscribe((items: Translation) => {
+          this.translations = items;
+        })
+    );
+
+    this.subscriptions.add(
+      this.actions$.pipe(ofType(ActionTypes.CREATE_MOVIE_SUCCESS)).subscribe(
+        (res: { payload: Movie }) => {
+          this.createMovieSuccess(res);
+        },
+        (err) => {
+          this.toastService.error(err.message);
+        }
+      )
+    );
+  }
+
+  createForm() {
+    return this.fb.group({
+      title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      poster: new FormControl(null),
+      genre: new FormControl('', [Validators.required]),
+      actors: new FormControl(null, Validators.required),
+      year: new FormControl(null, Validators.required),
+      duration: new FormControl(null, Validators.required),
+      imdbRating: new FormControl(null),
+    });
+  }
+
+  submitForm() {
+    this.formUtilsService.markFormControlsAsDirty(this.form);
+
+    if (this.form.valid) {
+      this.store.dispatch(requestCreateMovie({ payload: this.form.value }));
+    }
+  }
+
+  createMovieSuccess(res: { payload }) {
+    if (!res.payload) {
+      this.toastService.error(res.payload);
+    } else {
+      this.toastService.success(res.payload.title, this.translations.movieCreatedSuccess);
+      this.form.reset();
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
--- src/app/containers/movies/containers/movie-new/movie-new.module.ts
+++ src/app/containers/movies/containers/movie-new/movie-new.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { TranslocoModule, TRANSLOCO_SCOPE } from '@ngneat/transloco';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { MovieNewRoutingModule } from './movie-new-routing.module';
 import { MovieNewContainer } from './movie-new.container';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [MovieNewContainer],
-  imports: [CommonModule, MovieNewRoutingModule, FormsModule, ReactiveFormsModule],
-  providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'movie-new' }],
+  imports: [
+    CommonModule,
+    MovieNewRoutingModule,
+    TranslocoModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    MultiSelectModule,
+  ],
+  providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'movieNew' }],
 })
 export class MovieNewModule {}
