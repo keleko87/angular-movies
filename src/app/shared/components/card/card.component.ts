c0155c5237b6f706dbd83849b50ae4105fa783bc
mariom <mariom@empathy.co>
2021-05-14 14:02:31 +0200

    fix(card.component): Set lazy loading image. Integrate loader and default images

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -7,8 +7,27 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
   @Input() title: string;
-  @Input() image: string;
   @Input() footerItems: string[] = [];
 
-  constructor() {}
+  @Input() image: string;
+  @Input() imageLoader = '../../../../assets/img/image-cube.gif';
+  @Input() defaultImage = '../../../../assets/img/image-not-available.png';
+
+  isLoading: boolean;
+
+  constructor() {
+    this.isLoading = true;
+  }
+
+  get imageSrc() {
+    return this.isLoading ? this.imageLoader : this.image;
+  }
+
+  hideLoader() {
+    this.isLoading = false;
+  }
+
+  setDefaultImage() {
+    this.image = this.defaultImage;
+  }
 }
